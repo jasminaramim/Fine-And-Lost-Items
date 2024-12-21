6ff932cd82b0f8b967c3330615741c6ad50016c9
jasminaramim <jasminaramim2005@gmail.com>
2024-12-21 23:13:18 +0600

    login section done

--- src/main.jsx
+++ src/main.jsx
@@ -7,14 +7,15 @@ import { RouterProvider } from 'react-router-dom';
 import Router from './Routes/Router';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AuthProvider from './Providers/Authprovider';
 // import AuthProvider from './AuthContext/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <AuthProvider> */}
+    <AuthProvider>
       <RouterProvider router={Router}>
       </RouterProvider>
       <ToastContainer position='top-center' />
-    {/* </AuthProvider> */}
+    </AuthProvider>
   </StrictMode>
 );
